1dfe8db31dc6945545adfcbfb1326c0a304288c0
Serhii Melnychenko <higankingpro@gmail.com>
2022-02-16 20:23:20 +0200

    tasks 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,35 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const ulEl = document.querySelector('.gallery');
+
+
+createGallery(galleryItems);
+function createGallery(items) {
+    const markup = items.map((item) =>
+      
+        `<div class="gallery__item">
+ <a class="gallery__link"
+      href="${item.original}"
+       >
+         <img
+           class="gallery__image"
+           src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+       />
+      </a>
+</div>`
+  ).join('');
+    ulEl.insertAdjacentHTML('beforeend', markup);
+};
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+

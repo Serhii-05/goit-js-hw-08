9b26ea0079e94e86dd3341bae547791f1011161e
Serhii Melnychenko <higankingpro@gmail.com>
2022-02-20 20:12:00 +0200

    tasks 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from "lodash.throttle";
 
+const KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const formData = {};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+
+initForm();
+
+
+
+
+function initForm() {
+    const saveData = localStorage.getItem(KEY);
+    if (saveData) {
+        const { email, message } = JSON.parse(saveData);
+        form.email.value = email;
+
+        form.message.value = message;
+
+        formData.email = email;
+        formData.message = message;
+    }
+}
+
+function onFormInput(event) {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+  localStorage.setItem(KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formDataSend = new FormData(event.currentTarget);
+    formDataSend.forEach((value, name) => {
+        formData[name] = value;
+    });
+    event.currentTarget.reset();
+    localStorage.removeItem(KEY);
+
+    console.log(formData);
+}
